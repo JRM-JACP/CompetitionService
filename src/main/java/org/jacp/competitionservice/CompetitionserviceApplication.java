900b3f7516658cda33b9a9096e1ebbd8e9e04bdf
Aleksandr Alekseenko <alexan.sergeev@gmail.com>
2023-08-27 21:12:02 -0400

    fixing according to notes. Working on liquibase csv file format.

--- src/main/java/org/jacp/competitionservice/CompetitionserviceApplication.java
+++ src/main/java/org/jacp/competitionservice/CompetitionserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CompetitionserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CompetitionserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CompetitionserviceApplication.class, args);
+    }
 
 }
--- src/test/java/org/jacp/competitionservice/CompetitionserviceApplicationTests.java
+++ src/test/java/org/jacp/competitionservice/CompetitionserviceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CompetitionserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
