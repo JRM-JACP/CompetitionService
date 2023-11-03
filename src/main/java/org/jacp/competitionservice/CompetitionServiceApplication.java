d41ccf55fb5f2e6f1db17c1ef7cc1ea184235ce7
Aleksandr Alekseenko <alexan.sergeev@gmail.com>
2023-11-03 13:59:33 -0400

    JC-8. Corrections according Saffchen requirements.

--- src/main/java/org/jacp/competitionservice/CompetitionServiceApplication.java
+++ src/main/java/org/jacp/competitionservice/CompetitionServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CompetitionserviceApplication {
+public class CompetitionServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CompetitionserviceApplication.class, args);
+        SpringApplication.run(CompetitionServiceApplication.class, args);
     }
 
 }
