fc3d2c921b49838d4ab10e2b2273d5a3842a8d5f
Denis Savchenko <denis.savchenko98@yandex.ru>
2024-01-03 16:23:01 +0300

    Added SearchDto to receive data from the frontend

---
+++ src/main/java/org/jacp/dto/SearchDto.java
@@ -0,0 +1,19 @@
+package org.jacp.dto;
+
+import lombok.Data;
+import org.jacp.enums.Difficulty;
+import org.jacp.enums.Tags;
+
+import java.util.List;
+
+/**
+ * @author saffchen created on 20.12.2023
+ */
+@Data
+public class SearchDto {
+    private int limitTasks;
+    private Difficulty difficulty;
+    private Tags tagsList;
+    private List<ParticipantsDto> participantsDto;
+    private int duration;
+}
